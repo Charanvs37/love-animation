96ed39789e5ae4f72ddc78c728e3dcdc10edf6ea
Charanvs3 <2200031706@kluniversity.in>
2026-02-06 18:05:11 +0530

    Initial love animation upload

---
+++ I love you Animation/script.js
@@ -0,0 +1,51 @@
+// Floating Hearts
+const bg = document.querySelector(".hearts-bg");
+
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+  heart.innerHTML = "❤️";
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.fontSize = Math.random() * 20 + 10 + "px";
+  bg.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 6000);
+}
+
+setInterval(createHeart, 400);
+
+
+// Click Fireworks (mo.js)
+document.addEventListener('click', function(e) {
+  new mojs.Burst({
+    left: e.pageX,
+    top: e.pageY,
+    radius: { 0: 120 },
+    count: 20,
+    children: {
+      shape: 'circle',
+      fill: ['#ff4da6', '#ff1a75', '#ffffff'],
+      duration: 1500
+    }
+  }).play();
+});
+
+
+// Sound Toggle
+const soundBtn = document.querySelector(".sound");
+const music = document.getElementById("bgMusic");
+
+let isPlaying = false;
+
+soundBtn.addEventListener("click", () => {
+  if (!isPlaying) {
+    music.play();
+    soundBtn.innerText = "🔈 Playing";
+  } else {
+    music.pause();
+    soundBtn.innerText = "🔊 Sound";
+  }
+  isPlaying = !isPlaying;
+});
